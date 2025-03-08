f1872c29433850669d76466ef6a580fdcde19729
Hemant Shende <shendehc95@gmail.com>
2025-03-08 23:39:43 +0530

    added comments

--- src/Lambdas/LambadRunnable.java
+++ src/Lambdas/LambadRunnable.java
@@ -2,6 +2,9 @@ package Lambdas;
 
 public class LambadRunnable {
     public static void main(String[] args) {
+        //lambdas work with functional interface only
+        //func interface menas the interface with only one abstract method
+        //eg-runnable,collable, comparator,etc
         Runnable runnable=()->{
             System.out.println("Hello world");
         };
