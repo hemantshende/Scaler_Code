29b8c2985a7b1fdd6b82a9c6674262965795ee7c
Hemant Shende <shendehc95@gmail.com>
2025-03-10 14:32:40 +0530

    v0 of bird class..which is breaking SRP..

---
+++ src/SOLID_Principle/v0/Bird.java
@@ -0,0 +1,39 @@
+package SOLID_Principle.v0;
+
+//bird class is more generic
+//handling all the responsibility of all bird types
+//breaking single responsibility principle
+
+public class Bird {
+    private String name;
+    private String type;
+    int noOfWings;
+    boolean canFly;
+
+    public Bird(String name, String type, int noOfWings, boolean canFly) {
+        this.name = name;
+        this.type = type;
+        this.noOfWings = noOfWings;
+        this.canFly = canFly;
+    }
+
+    public void eat() {
+        System.out.println("bird is eating");
+    }
+
+    public void fly() {
+
+    }
+
+    public void makeSound() {
+        if(type.equals("sparrow")) {
+            System.out.println("gutu gutur");
+        }
+        else if(type.equals("peacock")) {
+            System.out.println("meeooo");
+        }else if(type.equals("crow")) {
+            System.out.println("cow cow");
+        }
+    }
+}
+
