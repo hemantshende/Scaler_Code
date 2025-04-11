66a17af7f8330c3d714fad8705d4e1f43ce8c178
Hemant Shende <shendehc95@gmail.com>
2025-04-11 12:26:52 +0530

    added gamecontroller with some methods..not implimented yet..

---
+++ src/Machine_Coding/TicTacToe/Controller/GameController.java
@@ -0,0 +1,26 @@
+package Machine_Coding.TicTacToe.Controller;
+
+import Machine_Coding.TicTacToe.Models.Game_State;
+import Machine_Coding.TicTacToe.Models.Player;
+
+public class GameController {
+    public void startGame(){
+
+    }
+    public void display(){
+
+    }
+    public void makeMove(){
+
+    }
+    public void undo(){
+
+    }
+    public Game_State gameState(){
+        return null;
+    }
+
+    public Player winner(){
+        return null;
+    }
+}
